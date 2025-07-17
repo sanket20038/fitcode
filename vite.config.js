a8f71bf5616bd0722f46e7c36417045d4fee22dc
Sanket Anant Patil <sanketapatil2003@gmail.com>
2025-07-18 00:21:24 +0530

    Frontend with production API URL

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
+    outDir: 'dist', // <--- THIS LINE IS MISSING
     rollupOptions: {
       external: [
         'pdfmake/build/pdfmake',
